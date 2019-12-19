4fdaec956898ae4d6a282ec011d824556aeda411
xuge <jixu.hjx@antfin.com>
2019-12-19 21:05:50 +0800

    gpg key

--- update-version.sh
+++ update-version.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/env bash
+
+version=$1
+
+./mvnw versions:set versions:set-scm-tag -DnewVersion=${version} -DnewTag=${version}
+./mvnw versions:commit
+git add pom.xml
+git commit -m "Bump version to ${version}"
+git tag -f -a -m "Release ${version}" "${version}"
