70712c67842e9a9d128d4b632b942f1659d5a234
xuge <jixu.hjx@antfin.com>
2019-12-19 16:41:17 +0800

    gpg key

---
+++ decrypt_secret.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Export key
+# gpg --quiet --batch --yes --export-secret-key --armor --output key.gpg ${GPG_KEY}
+
+# Encrypt key
+# gpg --quiet --batch --yes --passphrase="${GPG_PASSPHRASE}" --cipher-algo AES256 --output encrypt-key.gpg --symmetric key.gpg
+
+# Decrypt the file
+mkdir $HOME/secrets
+# --batch to prevent interactive command --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="${GPG_PASSPHRASE}" \
+--output $HOME/secrets/key.gpg encrypt-key.gpg
+gpg --import $HOME/secrets/key.gpg
